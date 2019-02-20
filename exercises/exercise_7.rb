1f2f59e51c4bb7cd7b5dbe94b424bb22c065b620
Christina <christina.hsu.ca@gmail.com>
2019-02-20 00:02:43 +0000

    add validation method

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please give me a name for the store you'd like to create"
+
+#ask user to create store name
+user_input = gets.chomp
+revenue = gets.chomp ## create this one to test the integer
+
+#create a store with user input & show error if input doesn't match criteria
+user_input = Store.create!(name: user_input, annual_revenue: revenue)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
-    belongs_to :store
+    belongs_to :store                       #give methods to the employee class.It's a helper method
+    validates :first_name, :presence => true
+    validates :last_name, :presence => true
+    validates :hourly_rate, :numericality => { :greater_than => 40 && less_than => 200}
+    validates :store, :presence => true     #validation for the foreign key
 end
+
+
